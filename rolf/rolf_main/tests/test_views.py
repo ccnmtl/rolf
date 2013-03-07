2a461359142e413e096d745698920e28baf2a043
Anders Pearson <anders@columbia.edu>
2013-03-07 11:23:26 -0500

    logged in user test

--- rolf/rolf_main/tests/test_views.py
+++ rolf/rolf_main/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
 
 
 class SimpleTest(TestCase):
@@ -9,3 +10,19 @@ class SimpleTest(TestCase):
     def test_root(self):
         response = self.c.get("/")
         self.assertEquals(response.status_code, 302)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.u = User.objects.create(username="testuser")
+        self.u.set_password("test")
+        self.u.save()
+
+    def tearDown(self):
+        self.u.delete()
+
+    def test_root(self):
+        self.c.login(username="testuser", password="test")
+        response = self.c.get("/")
+        self.assertEquals(response.status_code, 200)
