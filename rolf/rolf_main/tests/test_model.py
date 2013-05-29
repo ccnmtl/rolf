380d1588ddc17e4fcddca87eae493b15ef704909
Anders Pearson <anders@columbia.edu>
2013-05-29 10:30:29 -0400

    increase test coverage on models

--- rolf/rolf_main/tests/test_model.py
+++ rolf/rolf_main/tests/test_model.py
@@ -32,6 +32,9 @@ class ApplicationTest(unittest.TestCase):
         self.assertEquals(self.a.get_absolute_url(),
                           "/application/%d/" % self.a.id)
 
+    def test_active_deployments(self):
+        self.assertEquals(self.a.active_deployments().count(), 0)
+
 
 class DeploymentTest(unittest.TestCase):
     def setUp(self):
@@ -51,6 +54,42 @@ class DeploymentTest(unittest.TestCase):
         self.assertEquals(self.d.get_absolute_url(),
                           "/deployment/%d/" % self.d.id)
 
+    def test_all_categories(self):
+        self.assertEquals(self.d.all_categories().count(), 1)
+
+    def test_status(self):
+        self.assertEquals(self.d.status(), "unknown")
+
+    def test_last_message(self):
+        self.assertEquals(self.d.last_message(), None)
+
+    def test_all_recipes(self):
+        self.assertEquals(self.d.all_recipes().count(), 0)
+
+    def test_can_edit(self):
+        u = User.objects.create(username='testuserce')
+        self.assertFalse(self.d.can_edit(u))
+        u.delete()
+
+    def test_can_push(self):
+        u = User.objects.create(username='testuserce')
+        self.assertFalse(self.d.can_push(u))
+        u.delete()
+
+    def test_can_view(self):
+        u = User.objects.create(username='testuserce')
+        self.assertFalse(self.d.can_view(u))
+        u.delete()
+
+    def test_add_permission_form(self):
+        self.d.add_permission_form()
+
+    def test_add_flag_form(self):
+        self.d.add_flag_form()
+
+    def test_last_push_date(self):
+        self.assertEquals(self.d.last_push_date(), None)
+
 
 class BasicPushTest(unittest.TestCase):
     def setUp(self):
@@ -67,7 +106,7 @@ class BasicPushTest(unittest.TestCase):
         self.recipe = Recipe.objects.create(
             name="test recipe",
             language="python",
-            code="print 'hello'",
+            code="print 'hello'\nself.execute(['echo', 'foo'])",
             description="",
         )
         self.shell_recipe = Recipe.objects.create(
@@ -90,6 +129,7 @@ class BasicPushTest(unittest.TestCase):
     def tearDown(self):
         self.stage.delete()
         self.recipe.delete()
+        self.shell_recipe.delete()
         self.setting.delete()
         self.d.delete()
         self.a.delete()
@@ -107,3 +147,32 @@ class BasicPushTest(unittest.TestCase):
         # and let's make sure a setting variable has round-tripped
         ps = push.pushstage_set.get(stage=self.stage2)
         self.assertEquals(ps.stdout(), "TEST_BAR\n")
+        self.assertEquals(ps.stderr(), "")
+
+        self.assertEquals(self.d.most_recent_push(), push)
+        self.assertEquals(self.d.status(), push.status)
+        self.assertEquals(self.d.last_message(), push.comment)
+        self.assertEquals(self.d.last_push_date(), push.start_time)
+
+        self.assertEquals(push.get_absolute_url(),
+                          "/push/%d/" % push.id)
+
+        push.run_stage(self.stage.id, rollback_id=self.stage.id)
+
+        for pstage in push.reverse_pushstages():
+            self.assertEquals(pstage.setting('foo'), '')
+
+    def test_recipe_absolute_url(self):
+        self.assertEquals(
+            self.recipe.get_absolute_url(),
+            "/cookbook/%d/" % self.recipe.id)
+
+    def test_stage_absolute_url(self):
+        self.assertEquals(
+            self.stage.get_absolute_url(),
+            "/stage/%d/" % self.stage.id)
+
+    def test_stage_all_recipes(self):
+        self.assertEquals(
+            [r.id for r in self.stage.all_recipes()],
+            [self.shell_recipe.id])
