5c12aaa38cc03104ad9cd5884eda1c8a64443418
Anders Pearson <anders@columbia.edu>
2013-09-13 11:21:05 -0400

    wheelify

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import glob
 import os
 import sys
 import subprocess
@@ -19,18 +18,22 @@ ret = subprocess.call(["python", "virtualenv.py",
                        vedir])
 if ret: exit(ret)
 
+ret = subprocess.call(
+    [os.path.join(vedir, 'bin', 'pip'), "install",
+     "--index-url=http://pypi.ccnmtl.columbia.edu/",
+     "wheel==0.21.0"])
+
+if ret:
+    exit(ret)
+
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                       "-E", vedir,
-                       "--index-url=''",
-                       "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+                       "--use-wheel",
+                       "--index-url=http://pypi.ccnmtl.columbia.edu/",
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
 if ret: exit(ret)
-
-if sys.version_info < (2, 7, 0):
-    ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                           "-E", vedir,
-                           os.path.join(pwd,"requirements/src/importlib-1.0.1.tar.gz")])
-
 
 ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
+
+
