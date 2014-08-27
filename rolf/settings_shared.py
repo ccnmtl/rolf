8fe4b0a413ae73e9c8587cecd1e0f242308606f6
Anders Pearson <anders@columbia.edu>
2014-08-27 15:38:27 +0200

    switch to django 1.6 style atomic transactions

--- rolf/settings_shared.py
+++ rolf/settings_shared.py
@@ -19,6 +19,7 @@ DATABASES = {
         'PORT': 5432,
         'USER': '',
         'PASSWORD': '',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
@@ -79,7 +80,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
