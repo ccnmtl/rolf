1137783f1997abfbe8c6d823dc09d827afd10efd
Anders Pearson <anders@columbia.edu>
2017-06-14 10:52:16 +0100

    flake8 updates

--- rolf/rolf_main/admin.py
+++ rolf/rolf_main/admin.py
@@ -4,14 +4,20 @@ from models import Deployment, Category, Application
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Category, CategoryAdmin)
 
 
 class ApplicationAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Application, ApplicationAdmin)
 
 
 class DeploymentAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(Deployment, DeploymentAdmin)
